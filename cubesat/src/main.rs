87c1cd1fe537898d1f7cdf5a1add4b093a8cf23f
Phobia <filipe_msr@hotmail.com>
2023-09-11 18:43:50 -0300

    Chapter 4: CubeSat

---
+++ cubesat/src/main.rs
@@ -0,0 +1,80 @@
+#![allow(unused_variables)]
+
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    content: String,
+}
+
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>,
+}
+
+impl Mailbox {
+    fn post(&mut self, msg: Message) {
+        self.messages.push(msg);
+    }
+
+    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                let msg = self.messages.remove(i);
+                return Some(msg);
+            }
+        }
+
+        None
+    }
+}
+
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
+}
+
+impl CubeSat {
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        mailbox.deliver(&self)
+    }
+}
+
+struct GoundStation {}
+
+impl GoundStation {
+    fn send(&self, mailbox: &mut Mailbox, msg: Message) {
+        mailbox.post(msg);
+    }
+
+    fn connect(&self, sat_id: u64) -> CubeSat {
+        CubeSat { id: sat_id }
+    }
+}
+
+fn fetch_sat_ids() -> Vec<u64> {
+    vec![1, 2, 3]
+}
+
+fn main() {
+    let mut mail = Mailbox { messages: vec![] };
+
+    let base = GoundStation {};
+
+    let sat_ids = fetch_sat_ids();
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = Message {
+            to: sat_id,
+            content: String::from("Hello!"),
+        };
+        base.send(&mut mail, msg);
+    }
+
+    let sat_ids = fetch_sat_ids();
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+
+        let msg = sat.recv(&mut mail);
+        println!("{:?}: {:?}", sat, msg);
+    }
+}
